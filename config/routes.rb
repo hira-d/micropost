87420c06d96910cc546b7c9bb5e548a2602fb603
dai.hira <d.jacktennis@gmail.com>
2020-02-06 13:25:25 +0000

    kadai-micropost

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,25 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
-  
-  get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
-end
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create] do
+    member do
+      get :followings
+      get :followers
+      get :likes
+      get :microposts_fav
+    end
+  end
+  
+  resources :microposts, only: [:destroy, :create]
+
+  resources :relationships, only: [:create, :destroy]
+  
+  resources :favorites, only: [:create, :destroy]
+  
+    
+end
